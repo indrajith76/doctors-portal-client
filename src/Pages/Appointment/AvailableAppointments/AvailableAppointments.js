87cba1fc4c8e623e331928d5a8e7ee6bc7f85711
indrajith76 <indrajithgoswami@gmail.com>
2022-11-18 16:51:31 +0600

    added React Query to loaded data

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { format } from "date-fns";
 import AppointmentOption from "./AppointmentOption";
 import BookingModal from "../BookingModal/BookingModal";
+import { useQuery } from "@tanstack/react-query";
 
 const AvailableAppointments = ({ selectedDate }) => {
-  const [appointmentOptions, setAppointmentOptions] = useState([]);
   const [treatment, setTreatment] = useState(null);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/appointmentOptions")
-      .then((res) => res.json())
-      .then((data) => setAppointmentOptions(data));
-  }, []);
+  const {data:appointmentOptions = []} = useQuery({
+    queryKey: ["appointmentOptions"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/appointmentOptions");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  /* const { data: appointmentOptions = [] } = useQuery({
+    queryKey: ["appointmentOptions"],
+    queryFn: () =>
+      fetch("http://localhost:5000/appointmentOptions").then((res) =>
+        res.json()
+      ),
+  }); */
 
   return (
     <section className="my-20">
