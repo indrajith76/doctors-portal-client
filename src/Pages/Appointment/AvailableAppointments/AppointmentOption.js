dc77d7efb7d7874a586c6ac1fd5a5ee6916f0a7e
indrajith76 <indrajithgoswami@gmail.com>
2022-11-22 17:58:59 +0600

    add price on treatments

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -2,15 +2,21 @@ import React, { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthProvider";
 
 const AppointmentOption = ({ appointmentOption, setTreatment }) => {
-  const { name, slots } = appointmentOption;
+  const { name, slots, price } = appointmentOption;
+  console.log(price)
   const { isDark } = useContext(AuthContext);
   return (
     <div className={`card shadow-xl border ${isDark && "border-primary"}`}>
       <div className="card-body text-center">
         <h2 className="text-xl font-semibold text-secondary">{name}</h2>
-        <p className={`${isDark?'text-base-100':'text-accent'}`}>{slots.length > 0 ? slots[0] : "Try Another day"}</p>
-        <p className={`${isDark?'text-base-100':'text-accent'}`}>
+        <p className={`${isDark ? "text-base-100" : "text-accent"}`}>
+          {slots.length > 0 ? slots[0] : "Try Another day"}
+        </p>
+        <p className={`${isDark ? "text-base-100" : "text-accent"}`}>
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
+        </p>
+        <p className={`${isDark ? "text-base-100" : "text-accent"}`}>
+          <small>Price: ${price}</small>
         </p>
         <div className="card-actions justify-center">
           <label
